2510783518dc3dc31399d2695911fd760d0c302e
Luciferkunal16 <Kunalbatham15@gmail.com>
2022-01-27 18:29:52 +0530

    Added UC10: search on the basis of City

--- AddressBook/contact.py
+++ AddressBook/contact.py
@@ -2,20 +2,26 @@ import logging
 
 
 class Contact:
-    def __init__(self, first_name, last_name, city, state, zip, phone_number, email):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.city = city
-        self.state = state
-        self.zip = zip
-        self.phone_number = phone_number
-        self.email = email
+    def __init__(self, contact_obj={}):
+        self.first_name = contact_obj.get("first_name")
+        self.last_name = contact_obj.get("last_name")
+        self.city = contact_obj.get("city")
+        self.state = contact_obj.get("state")
+        self.zip = contact_obj.get("zip")
+        self.phone_number = contact_obj.get("phone_number")
+        self.email = contact_obj.get("email")
 
-    def get_all_detail(self):
+    def get_all_details(self):
         """
 
-        :return: String
+        :return: String of details
         """
         return "First Name is {}".format(self.first_name), "Last name is {}".format(self.last_name), "Ciy is {}".format(
             self.city), "State is {} ".format(self.state), "Zip Code is {} ".format(
             self.zip), "Phone Number is {}".format(self.phone_number), "Email is {}".format(self.email)
+
+    def get_city(self):
+        return self.city
+
+    def get_name(self):
+        return "NAME is {} {}".format(self.first_name,self.last_name)
